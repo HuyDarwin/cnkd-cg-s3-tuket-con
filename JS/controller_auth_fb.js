77f44557dc7e1a42ac5eba15dda1d5057f411286
HuyDarwin <95059688+HuyDarwin@users.noreply.github.com>
2025-07-18 18:02:39 +0700

    tyyf

---
+++ JS/controller_auth_fb.js
@@ -0,0 +1,86 @@
+import { getDatabase, ref, set, update, onValue, remove, get, child } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+$(function () {
+	"use strict";
+
+	window.CNKDCGV = window.CNKDCGV || {};
+
+	(function (con) {
+		const db = getDatabase();
+        const dbKey = "cnkdcgv";
+
+        //
+
+        function upd_passkey(key, val) {
+            update(ref(db, dbKey + "_passkey"), {
+                [key]: val
+            })
+        }
+
+        function enb(key) {
+            $(key).removeAttr('disabled')
+        }
+
+        function dib(key) {
+            $(key).attr('disabled', true);
+        }
+      
+        // Popup - Passkey
+      
+        const player_url = "player-dote.html";
+        const host_url = "host-ore.html";
+      
+        $(".popup-close").click(function(){
+          $(".popup").css("top", "1920px");
+        });
+      
+        $(".edit-passkey").click(function(){
+          $(".popup").css("top", "1920px");
+          $("#popup-passkey").css("top", "0px");
+        });
+      
+        $(".ppp-input").keyup(function(e){
+          if((e.keyCode || e.which) == 13) { 
+            var id = "#" + $(this).parent().attr("id");
+            var pass_inp = $(this).val();
+            pass_inp = pass_inp.slice(0, -1);
+            $(id + " .ppp-input").val(pass_inp);
+            $(id + " .ppp-submit").click();
+          }
+        });
+      
+        $(".ppp-random").click(function(){
+          var id = "#" + $(this).parent().attr("id");
+          var who = id.substr(5);
+          var passkey = String.fromCharCode(getRandomIntInclusive(97,122),getRandomIntInclusive(97,122),getRandomIntInclusive(97,122),getRandomIntInclusive(97,122),getRandomIntInclusive(97,122),getRandomIntInclusive(97,122));
+          
+          upd_passkey(who + "_passkey", passkey);
+        });
+      
+        $(".ppp-submit").click(function(){
+          var id = "#" + $(this).parent().attr("id");
+          var who = id.substr(5);
+          var passkey = $(id + " .ppp-input").val();
+          upd_passkey(who + "_passkey", passkey);
+        });
+      
+        upd_passkey("player_url", player_url);
+        upd_passkey("host_url", host_url);
+      
+        onValue(ref(db, dbKey + "_passkey"), (snapshot) => {
+            const data = snapshot.val();
+          
+            var pp_copy_string = "";
+            for (var i = 1; i <= 6; i++) {
+              con.TextUpdateData("#ppp_player_" + i + " .ppp-pass", eval("data.player_" + i + "_passkey"), 1);
+              pp_copy_string += "Mã NC" + i + ": " + eval("data.player_" + i + "_passkey") + "\n";
+            }
+            con.TextUpdateData("#ppp_host .ppp-pass", data.host_passkey, 1);
+            pp_copy_string += "Mã DCT: " + data.host_passkey + "\n";
+            $(".popup-passkey-copy").html(pp_copy_string);
+        });
+
+		//		
+
+	}(window.CNKDCGV = window.CNKDCGV || {}));
+});
--- JS/player_fb.js
+++ JS/player_fb.js
@@ -7,66 +7,68 @@ $(function () {
 
 	(function (con) {
 		const db = getDatabase();
-		
+
 		// Get data
-		
+
 		onValue(ref(db, 'variables'), (snapshot) => {
 			const data = snapshot.val();
-			
-			if(data.buzzer_number != 0 && data.buzzer_number != undefined){
+
+			if (data.buzzer_number != 0 && data.buzzer_number != undefined) {
 				$('.who_buzzers').html(data.buzzer_number + '. ' + eval('data.contestant_' + data.buzzer_number + '_name'));
 			}
-			else{
+			else {
 				$('.who_buzzers').html('');
 			}
-			
-			if(data.buzzer_toggle == 0 || data.buzzer_toggle == undefined){
+
+			if (data.buzzer_toggle == 0 || data.buzzer_toggle == undefined) {
 				$('.buzzer').attr('disabled', true);
 			}
-			if(data.buzzer_toggle == 1){
+			if (data.buzzer_toggle == 1) {
 				$('.buzzer').removeAttr('disabled');
 			}
 		})
-		
+
 		onValue(ref(db, 'commands'), (snapshot) => {
 			const data = snapshot.val();
-			
-			if (data.reload == 1){
+
+			if (data.reload == 1) {
 				location.reload(true);
-				update(ref(db, 'commands'), { reload : 0 });
+				update(ref(db, 'commands'), { reload: 0 });
 			}
-			
+
 		})
-		
+
 		// Button Function
-		
-		$('.buzzer').click(function(){
+
+		$('.buzzer').click(function () {
 			$('.buzzer').attr('disabled', true);
 			update(ref(db, 'variables'), {
 				buzzer_status: 1,
 				buzzer_number: number_of_player
 			})
 		})
-		
+
 		// Action
-		
+
+		$(".order").html(number_of_player);
+
 		$('.who_buzzers').attr('disabled', true);
-    
-    $('.prev_wheel').click(function(){
-      $('.prev_wheel, .prev_puzzle').attr('disabled', true);
-      $('.prev_puzzle').removeAttr('disabled');
-      $('.preview').css('opacity',0);
-      $('#preview_1').css('opacity',1);
-    });
-    
-    $('.prev_puzzle').click(function(){
-      $('.prev_wheel, .prev_puzzle').attr('disabled', true);
-      $('.prev_wheel').removeAttr('disabled');
-      $('.preview').css('opacity',0);
-      $('#preview_2').css('opacity',1);
-    });
-    
-    $('.prev_wheel').click();
-		
+
+		$('.prev_wheel').click(function () {
+			$('.prev_wheel, .prev_puzzle').attr('disabled', true);
+			$('.prev_puzzle').removeAttr('disabled');
+			$('.preview').css('opacity', 0);
+			$('#preview_1').css('opacity', 1);
+		});
+
+		$('.prev_puzzle').click(function () {
+			$('.prev_wheel, .prev_puzzle').attr('disabled', true);
+			$('.prev_wheel').removeAttr('disabled');
+			$('.preview').css('opacity', 0);
+			$('#preview_2').css('opacity', 1);
+		});
+
+		$('.prev_wheel').click();
+
 	}(window.CNKDCGV = window.CNKDCGV || {}));
 });
