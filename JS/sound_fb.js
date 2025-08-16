4f54e0105392884a8a319e27d08e1cfe4b137ea3
HuyDarwin <95059688+HuyDarwin@users.noreply.github.com>
2025-08-16 21:57:13 +0700

    hgfy

--- JS/sound_fb.js
+++ JS/sound_fb.js
@@ -323,7 +323,7 @@ $(function () {
               con.PlaySound("Assets/tuyệt%20đỉnh%20song%20ca.mp3", 1)
 				update(ref(db, 'commands'), { sound_cus_7: 0 });
             }
-            if(data.sound_cus_7 == 1){
+            if(data.sound_cus_8 == 1){
               con.PlaySound("Assets/y2mate.com - Background Suspense Music  Suspenseful  Dramatic Film Soundtracks ANTICIPATION.mp3", 3)
 				update(ref(db, 'commands'), { sound_cus_8: 0 });
             }
